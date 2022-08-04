587655874233382fb960aef56a716f8ca374ecb4
Yoonseong Jung <ohikendoit@gmail.com>
2022-08-04 16:48:11 +0900

    [Bronze III] Title: TGN, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,11 @@
+test_case = int(input())
+
+for i in range(test_case):
+  r, e, c = map(int, input().split())
+
+  if r > e-c:
+    print('do not advertise')
+  elif r == e-c:
+    print('does not matter')
+  else:
+    print('advertise')
